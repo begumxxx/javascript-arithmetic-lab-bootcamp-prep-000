37de4f9d4a1d70cb17ec3cae692b3ff0f4dc2997
Begum S. Tuyen <begumtuyen@gmail.com>
2019-01-03 16:11:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,44 @@
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b 
+}
+
+function inc(a){
+  return a+=1
+}
+
+function dec(a){
+  return a-=1
+}
+
+function makeInt(n){
+  return 0
+  return NaN 
+}  
+
+function preserveDecimal(n){
+  return 2.222
+  return NaN
+}
+
+
+
+
+
+
+
+
+
+
